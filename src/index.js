4e76b21b19498862bc1db5656b68566b56e75dd2
MuhammedHashimT <hashimt4567@gmail.com>
2022-08-09 22:02:41 +0530

    pwa rwq

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-
 import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 ReactDOM.render(
@@ -17,5 +17,10 @@ ReactDOM.render(
 );
 
 reportWebVitals();
+//  If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
 
 
