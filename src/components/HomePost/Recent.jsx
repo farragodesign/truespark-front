bc6a5e6e3328a2d7a09620fc6e9d639fac5e92d8
FarragoCommunity <102537372+FarragoCommunity@users.noreply.github.com>
2022-12-18 16:10:56 +0530

    Update Recent.jsx

--- src/components/HomePost/Recent.jsx
+++ src/components/HomePost/Recent.jsx
@@ -10,7 +10,7 @@ import 'aos/dist/aos.css';
 function Recent() {
     const [data, setData] = useState(null);
     useEffect(() => {
-        axios.get("https://truespark.herokuapp.com/api/v1/articles/?limit=4").then((res) => {
+        axios.get("https://kind-ruby-binturong-boot.cyclic.app/api/v1/articles/?limit=4").then((res) => {
             setData(res.data.blogs);
         });
 
